e6a0050cb14d39624c1d96c639c0484588ad616c
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2023-04-07 15:23:02 -0600

    Add SafeArea widget example usage and remove the debug banner

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-
-import '../widgets/circle_container.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("My First Flutter App"),
-      ),
-      body: Center(
-        child: CircleContainer(
-            child: SvgPicture.asset(
-              'assets/icons/menu-icon.svg',
-              width: 100,
-              height: 100,
-            ),
-            width: 150,
-            height: 150),
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        top: true,
+        bottom: true,
+        child: Container(
+          alignment: Alignment.bottomCenter,
+          child: Text('Hello World!', style: TextStyle(fontSize: 24)),
+        ),
       ),
     );
   }
